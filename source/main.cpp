6170d00a095bb534b3df70c08a794333f646742f
Brandon Quintin <brandonleequintin@gmail.com>
2024-02-08 12:24:46 -0500

    Added Floor Object, Reorganized Code
    Boxes and Floors have separate VAO declarations.
    
    Added main.cpp comments, while at the same time moving rarely changed code to the top of the program.

--- source/main.cpp
+++ source/main.cpp
@@ -17,7 +17,7 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/string_cast.hpp>
 
-// variables
+// variables (may also contain functions)
 #include "globals.h"
 #include "opengl/camera_variables.h"
 #include "shapes/shapes.h" // stores both shape vertices AND initial object coordinates
@@ -32,25 +32,12 @@ int main(){
     glEnable(GL_DEPTH_TEST);
     Shader mainShader("shaders/shader.vs", "shaders/shader.fs");
 
-    // initialize box positions
-    generateSurroundingPositions(initialBoxPositions, 25);
-    float s = 10.0f;
-    glm::vec3 scaleFactor(s, s, s);
-    for (int i = 0; i < initialBoxPositions.size(); i++){
-        initialBoxPositions[i] = initialBoxPositions[i] * scaleFactor;
-    }
-
-    // initialize 25 boxes
-    shape boxes[25];
-    int boxesArrSize = sizeof(boxes) / sizeof(boxes[1]);
-    for (int i = 0; i < boxesArrSize; i++){
-        boxes[i].modelMatrix = glm::translate(boxes[i].modelMatrix, initialBoxPositions[i]);
-    }
-
-    // ----- BOX VERTICES BUFFERS ------
+    // ----- OBJECTS VERTICES BUFFERS ------
     unsigned int boxVAO, boxVBO;
     generateBoxVAO(boxVAO, boxVBO, boxVerticesByteSize, boxVertices);
 
+    unsigned int floorVAO, floorVBO, floorEBO;
+    generateFloorVAO(floorVAO, floorVBO, floorEBO, floorVerticesByteSize, floorVertices);
 
     // ----- TEXTURES -----
     unsigned int texture1, texture2;
@@ -67,6 +54,32 @@ int main(){
 
     mainShader.setMat4("view", view);
 
+    // ----- INITIALIZE OBJECTS -----
+
+    // initialize various box positions
+    generateSurroundingPositions(initialBoxPositions, 25);
+    float s = 10.0f;
+    glm::vec3 scaleFactor(s, s, s);
+    for (int i = 0; i < initialBoxPositions.size(); i++){
+        initialBoxPositions[i] = initialBoxPositions[i] * scaleFactor;
+    }
+
+    // initialize 25 boxes
+    shape boxes[25];
+    int boxesArrSize = sizeof(boxes) / sizeof(boxes[0]);
+    for (int i = 0; i < boxesArrSize; i++){
+        boxes[i].modelMatrix = glm::translate(boxes[i].modelMatrix, initialBoxPositions[i]);
+    }
+
+    // initialize 1 floor
+    shape floors[1];
+    floors[0].type = 2;
+    int floorsArrSize = sizeof(floors) / sizeof(floors[0]);
+    floors[0].modelMatrix = glm::translate(floors[0].modelMatrix, glm::vec3(0.0f, -1.0f, 0.0f));
+    floors[0].modelMatrix = glm::scale(floors[0].modelMatrix, glm::vec3(15.0f, 0.0f, 15.0f));
+
+
+    // ----- MAIN PROGRAM -----
 
     while (!glfwWindowShouldClose(window))
     {
@@ -83,11 +96,22 @@ int main(){
         mainShader.setMat4("projection", projection);
 
 
+        // ----- DRAW OBJECTS ------
+
         for (int i = 0; i < boxesArrSize; i++){
+            glBindVertexArray(boxVAO);
             mainShader.setMat4("model", boxes[i].modelMatrix);
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
+        for (int i = 0; i < floorsArrSize; i++){
+            glBindVertexArray(floorVAO);
+            mainShader.setMat4("model", floors[i].modelMatrix);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, floorEBO);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        }
 
+
+        // ----- END OF PROGRAM -----
 
         glfwSwapBuffers(window);
         glfwPollEvents();
--- source/shapes/shapes.h
+++ source/shapes/shapes.h
@@ -5,16 +5,20 @@
 
 struct shape {
     glm::mat4 modelMatrix = glm::mat4(1.0f); // initialized to an identity matrix
+    // UNUSED FOR NOW
     unsigned int type = 0; // 1 = box, 2 = floor
+
 };
 
 extern float boxVertices[];
 extern unsigned int boxVerticesByteSize;
 
 extern float floorVertices[];
+extern unsigned int floorIndices[];
 extern unsigned int floorVerticesByteSize;
 
 extern std::vector<glm::vec3> initialBoxPositions;
 
 void generateSurroundingPositions(std::vector<glm::vec3> &inputVector, int boxCount);
 void generateBoxVAO(unsigned int &VAO, unsigned int &VBO, unsigned int boxVerticesByteSize, float boxVertices[]);
+void generateFloorVAO(unsigned int &VAO, unsigned int &VBO, unsigned int &EBO, unsigned int floorVerticesByteSize, float floorVertices[]);
